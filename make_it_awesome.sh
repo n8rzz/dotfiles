181a0f5418832196c14f8a47e9c6dd31173c8576
Nate Geslin <teamtomkins23@gmail.com>
2016-08-22 09:15:23 -0500

    Remove backslash in front of curl command

--- make_it_awesome.sh
+++ make_it_awesome.sh
@@ -86,7 +86,7 @@ apm link ~/Documents/www/ss-orca-ui
 # rvm / ruby
 rvm / ruby / bundler
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
+curl -sSL https://get.rvm.io | bash -s stable --ruby
 
 # ruby gems
 gem install bundler rails
